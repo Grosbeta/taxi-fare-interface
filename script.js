269ed678113f70c50bee8be80e54cb086f4c76b9
antonindavid <antonin.david@edu.escp.eu>
2023-08-25 01:32:45 +0200

    My first website

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifareyeah-7dsq33jgta-ew.a.run.app/predict'
+//'https://taxifareyeah-7dsq33jgta-ew.a.run.app/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ3Jvc2JldGEyIiwiYSI6ImNsbHFldnJodjBkaGMzZXBwczJmYTluYnQifQ.l0Fw0anG2b3xj7sh9fEtqg';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
@@ -210,7 +211,7 @@ const predict = () => {
       .then(data => {
         document.getElementById('fare').classList.remove('d-none');
         const fareResult = document.getElementById('predicted-fare');
-        const fare = Math.round(data['fare'] * 100) / 100
+        const fare = Math.round(data['fare_amount'] * 100) / 100
         fareResult.innerText = `$${fare}`;
       })
       .catch((error) => {
